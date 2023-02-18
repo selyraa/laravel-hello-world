8da5e66a69590aa6a1f949b7c05a5059e6eae90f
selyraa <selyra.123@gmail.com>
2023-02-18 10:41:38 +0700

    Melakukan edit pada file welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,6 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <!-- <link rel="preconnect" href="https://fonts.bunny.net"> -->
-        <!-- <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" /> -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
